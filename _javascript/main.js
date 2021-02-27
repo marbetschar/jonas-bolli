c094b79d38a6e9eaa44c6849db4807f2f039668f
Marco Betschart <email@marco.betschart.name>
2021-02-27 13:11:16 +0100

    Initial basics

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+  let containerVideo = document.getElementById('container-video');
+  let navbarVideo = document.getElementById('navbar-video');
+
+  if (containerVideo && navbarVideo) {
+    var videos = containerVideo.getElementsByTagName('video');
+    var anchors = navbarVideo.getElementsByTagName('a');
+
+    if (videos.length != anchors.length) {
+      console.error ("video-count does not match anchor-count.");
+      return;
+    } else if (!videos.length) {
+      console.error ("No videos available.");
+      return;
+    }
+
+    anchors[0].classList.add('is-active');
+    videos[0].classList.remove('is-not-visible');
+    videos[0].load();
+
+    for (let anchor of anchors) {
+      anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        for (var k = 0; k < videos.length; k++) {
+          if (anchors[k] == e.target) {
+            if (!anchors[k].classList.contains('is-active')) {
+              anchors[k].classList.add('is-active');
+              videos[k].classList.remove('is-not-visible');
+              videos[k].load();
+            }
+
+          } else {
+            anchors[k].classList.remove('is-active');
+            videos[k].classList.add('is-not-visible');
+          }
+        }
+      });
+    }
+  }
 });
